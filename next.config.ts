e6c7e2c8fd93815c9dfae43e83089478f8ea6167
Dino0204 <s24020@gsm.hs.kr>
2025-10-14 20:58:32 +0900

    feat: reactCompiler

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingRoot: __dirname,
+  experimental: {
+    reactCompiler: true,
+  },
 };
 
 export default nextConfig;
