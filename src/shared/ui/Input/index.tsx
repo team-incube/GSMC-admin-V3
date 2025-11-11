699961507adc4a93c7c53e4fe1a6965a64c27763
Dino0204 <s24020@gsm.hs.kr>
2025-11-11 20:32:29 +0900

    fix: module not found

---
+++ src/shared/ui/Input/index.tsx
@@ -0,0 +1,17 @@
+interface InputProps {
+  label?: string;
+}
+
+export default function Input({
+  label,
+  ...props
+}: InputProps & React.InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <div className="flex flex-col gap-1">
+      <label className="text-body1 text-main-700" htmlFor={label}>
+        {label}
+      </label>
+      <input className="rounded-2xl border border-gray-300 p-4" {...props} id={label} type="text" />
+    </div>
+  );
+}
