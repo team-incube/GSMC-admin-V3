3039a12374474286b0c2e2035750840f9f40dbbe
Dino0204 <s24020@gsm.hs.kr>
2025-11-11 21:16:05 +0900

    feat: header

---
+++ src/app/(with-header)/layout.tsx
@@ -0,0 +1,12 @@
+import Header from '@/shared/ui/header/ui/Header';
+
+export default function Mainlayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Header />
+      <div className="flex justify-center px-4">
+        <main className="flex w-full max-w-[600px]">{children}</main>
+      </div>
+    </>
+  );
+}
