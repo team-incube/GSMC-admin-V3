36df963c4c15d4dcacadbed689cd003054a9923b
Dino0204 <s24020@gsm.hs.kr>
2025-11-12 11:58:22 +0900

    feat: filter

---
+++ src/shared/asset/svg/Filter.tsx
@@ -0,0 +1,21 @@
+export default function Filter() {
+  return (
+    <svg
+      width={32}
+      height={32}
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid meet"
+    >
+      <path
+        d="M6.66699 5.33301H25.3337L18.667 13.9997V26.6663L13.3337 21.333V13.9997L6.66699 5.33301Z"
+        stroke="black"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
