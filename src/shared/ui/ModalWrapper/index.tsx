1954a07891f93b90a4c92244575b834752994035
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-01 23:36:04 +0900

    refactor: ModalWrapper에 커스텀 스타일 기능 추가

--- src/shared/ui/ModalWrapper/index.tsx
+++ src/shared/ui/ModalWrapper/index.tsx
@@ -1,7 +1,15 @@
-export default function ModalWrapper({ children }: { children: React.ReactNode }) {
+export default function ModalWrapper({
+  children,
+  className = '',
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-[rgba(17,17,17,0.20)]">
-      <div className="relative z-10 rounded-[20px] bg-white px-10 py-6">{children}</div>
+      <div className={`relative z-10 rounded-[20px] bg-white ${className || 'px-10 py-6'}`}>
+        {children}
+      </div>
     </div>
   );
 }
