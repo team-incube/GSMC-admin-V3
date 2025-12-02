4a4919e8d68a0c0d1a13f03491910c476f8a4343
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-02 16:59:16 +0900

    style: MemberView 코드 Prettier 포맷팅 적용

--- src/view/member/ui/MemberView/index.tsx
+++ src/view/member/ui/MemberView/index.tsx
@@ -1,37 +1,31 @@
-import Filter from "@/shared/asset/svg/Filter";
-import QuestionMark from "@/shared/asset/svg/QuestionMark";
+import Filter from '@/shared/asset/svg/Filter';
+import QuestionMark from '@/shared/asset/svg/QuestionMark';
 
 export default function MemberView() {
   return (
-    <div className="flex gap-7.5 mt-12.5">
-      <div className="w-143.25 h-184.75 flex flex-col rounded-2xl bg-main-100 overflow-hidden">
+    <div className="mt-12.5 flex gap-7.5">
+      <div className="bg-main-100 flex h-184.75 w-143.25 flex-col overflow-hidden rounded-2xl">
         <div className="flex items-center justify-between px-10 pt-9 pb-6">
           <div className="flex items-baseline gap-1">
-            <p className="text-2xl font-semibold text-main-700">216</p>
+            <p className="text-main-700 text-2xl font-semibold">216</p>
             <p className="text-base font-semibold text-black">명</p>
           </div>
           <Filter />
         </div>
 
-        <div className="flex flex-col gap-0 px-5.5 flex-1 overflow-y-auto">
-          <article className="flex flex-col rounded-xl bg-main-100">
-            <div className="flex justify-between items-center px-8 py-6 rounded-xl">
-              <p className="text-lg font-semibold text-gray-600">
-                모태환
-              </p>
-              <p className="text-lg font-semibold text-gray-600">
-                2104
-              </p>
+        <div className="flex flex-1 flex-col gap-0 overflow-y-auto px-5.5">
+          <article className="bg-main-100 flex flex-col rounded-xl">
+            <div className="flex items-center justify-between rounded-xl px-8 py-6">
+              <p className="text-lg font-semibold text-gray-600">모태환</p>
+              <p className="text-lg font-semibold text-gray-600">2104</p>
             </div>
           </article>
         </div>
       </div>
 
-      <div className="w-87.5 h-184.75 flex flex-col items-center justify-center rounded-2xl bg-main-100 overflow-hidden">
+      <div className="bg-main-100 flex h-184.75 w-87.5 flex-col items-center justify-center overflow-hidden rounded-2xl">
         <QuestionMark />
-        <p className="mt-8 text-2xl font-semibold text-center text-gray-600">
-          학생을 선택해주세요
-        </p>
+        <p className="mt-8 text-center text-2xl font-semibold text-gray-600">학생을 선택해주세요</p>
       </div>
     </div>
   );
