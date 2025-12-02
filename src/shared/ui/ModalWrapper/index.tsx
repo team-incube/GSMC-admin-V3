b07c3a77193fafa65334bbe5417b7ac4ab527e59
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-02 17:26:05 +0900

    feat: ModalWrapper에 cn 적용으로 tailwind css class 우선 순위 정리

--- src/shared/ui/ModalWrapper/index.tsx
+++ src/shared/ui/ModalWrapper/index.tsx
@@ -1,3 +1,5 @@
+import { cn } from '@/shared/lib/cn';
+
 export default function ModalWrapper({
   children,
   className = '',
@@ -7,7 +9,7 @@ export default function ModalWrapper({
 }) {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-[rgba(17,17,17,0.20)]">
-      <div className={`relative z-10 rounded-[20px] bg-white ${className || 'px-10 py-6'}`}>
+      <div className={cn('relative z-10 rounded-[20px] bg-white px-10 py-6', className)}>
         {children}
       </div>
     </div>
