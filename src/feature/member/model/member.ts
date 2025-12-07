4cf587dfaf0bed980a648e3756f4baab9b3016f9
moonkanghyeon <s25024@gsm.hs.kr>
2025-12-07 14:36:54 +0900

    refactor: 미사용 파일 제거 (member.ts, getMembers.ts)

--- src/feature/member/model/member.ts
+++
@@ -1,9 +0,0 @@
-export interface Member {
-  id: number;
-  name: string;
-  email: string;
-  grade: number;
-  classNumber: number;
-  number: number;
-  role: 'UNAUTHORIZED' | 'STUDENT' | 'TEACHER' | 'ROOT';
-}
