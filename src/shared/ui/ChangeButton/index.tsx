3b3bba276fd98d56e2be928b2abc1e2cb71ab011
jyuuuuu0 <s25023@gsm.hs.kr>
2025-12-14 03:52:11 +0900

    feat: 변경 버튼 생성

---
+++ src/shared/ui/ChangeButton/index.tsx
@@ -0,0 +1,9 @@
+export default function ChangeButton() {
+  return (
+    <div>
+      <button className="border-main-500 text-main-500 h-[34px] w-[56px] rounded-lg border text-lg font-semibold">
+        변경
+      </button>
+    </div>
+  );
+}
