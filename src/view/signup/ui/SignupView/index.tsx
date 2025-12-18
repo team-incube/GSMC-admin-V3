61f4a2490d40697200ad6ad001d88c5c84326b15
Dino0204 <s24020@gsm.hs.kr>
2025-12-18 16:16:40 +0900

    feat: 뒤로가기

--- src/view/signup/ui/SignupView/index.tsx
+++ src/view/signup/ui/SignupView/index.tsx
@@ -1,11 +1,14 @@
 import BackArrow from '@/shared/asset/svg/BackArrow';
 import SignupForm from '@/widget/signup/ui';
+import { useRouter } from 'next/navigation';
 
 export default function SignupView() {
+  const router = useRouter();
+
   return (
     <div className="flex justify-center px-6">
       <div className="flex w-full max-w-[600px] flex-col gap-3">
-        <header className="flex items-center pt-[57.5px] pb-[75.5px]">
+        <header className="flex items-center pt-[57.5px] pb-[75.5px]" onClick={() => router.push('/')}>
           <BackArrow />
           <h2 className="text-main-700 text-titleSmall">내 정보 등록</h2>
         </header>
