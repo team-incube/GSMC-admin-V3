a959011a8c5ac2edfc32869e386de0b57f90a1de
Dino0204 <s24020@gsm.hs.kr>
2025-12-23 03:07:48 +0900

    feat: score

--- src/feature/member/api/getTotalScore.ts
+++ src/feature/member/api/getTotalScore.ts
@@ -1,6 +1,7 @@
+import { TotalScoreType } from '@/entities/score/model/score';
 import { instance } from '@/shared/lib/instance';
 
-export const getTotalScore = async (memberId: number) => {
+export const getTotalScore = async (memberId: number): Promise<TotalScoreType> => {
   const res = await instance.get(`/scores/total/${memberId}`, {
     params: {
       includeApproveOnly: true,
