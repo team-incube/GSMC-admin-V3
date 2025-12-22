525f45bb61f15337428c60565e86e89ac4a710f5
Dino0204 <s24020@gsm.hs.kr>
2025-12-23 02:10:27 +0900

    change: main redirect page

--- src/widget/header/ui/index.tsx
+++ src/widget/header/ui/index.tsx
@@ -17,11 +17,11 @@ export default function Header() {
     <>
       <header className="flex h-[70px] w-full items-center justify-center border-b border-gray-100 bg-white sticky top-0 z-50">
         <div className="flex w-150 flex-shrink-0 flex-grow-0 items-center justify-between px-3 text-center">
-          <Link href="/main" className="text-main-800 text-xl font-bold">
+          <Link href="/member" className="text-main-800 text-xl font-bold">
             GSMC
           </Link>
 
-          <nav className="flex mx-17 w-full justify-end items-center gap-8 text-sm">
+          <nav className="flex mx-17 w-full justify-between items-center gap-8 text-sm">
             {HEADER_NAV.map((item) => (
               <Link key={item.path} className="text-gray-500" href={item.path}>
                 {item.label}
