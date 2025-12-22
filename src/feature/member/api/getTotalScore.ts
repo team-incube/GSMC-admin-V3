5e97c1c101d2a031e5b1192279809ab8767ddea5
Dino0204 <s24020@gsm.hs.kr>
2025-12-23 03:28:03 +0900

    chore: totalScore 반환값 수정

--- src/feature/member/api/getTotalScore.ts
+++ src/feature/member/api/getTotalScore.ts
@@ -2,10 +2,10 @@ import { TotalScoreType } from '@/entities/score/model/score';
 import { instance } from '@/shared/lib/instance';
 
 export const getTotalScore = async (memberId: number): Promise<TotalScoreType> => {
-  const res = await instance.get(`/scores/total/${memberId}`, {
+  const response = await instance.get(`/scores/total/${memberId}`, {
     params: {
       includeApproveOnly: true,
     },
   });
-  return res.data?.data.totalScore ?? 0;
+  return response.data.data;
 };
