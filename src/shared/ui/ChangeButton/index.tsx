47f08882998275fcc8f07ace4c821fa05a97eecc
Dino0204 <s24020@gsm.hs.kr>
2025-12-23 03:52:10 +0900

    chore: Button을 사용하도록 변경

--- src/shared/ui/ChangeButton/index.tsx
+++ src/shared/ui/ChangeButton/index.tsx
@@ -7,7 +7,7 @@ export default function ChangeButton({ onClick }: ChangeButtonProps) {
     <button
       type="button"
       onClick={onClick}
-      className="border-main-500 text-main-500 h-[34px] w-[56px] rounded-lg border text-lg font-semibold"
+      className="cursor-pointer border-main-500 text-main-500 h-[34px] w-[56px] rounded-lg border text-lg font-semibold"
     >
       변경
     </button>
