8d0c00ae8de1037ac9df3848d1e114c06acd2fa0
Dino0204 <s24020@gsm.hs.kr>
2025-12-23 03:52:27 +0900

    fix: 다른 status의 값도 호출할 수 있도록 변경

--- src/feature/member/api/getScoresByCategory.ts
+++ src/feature/member/api/getScoresByCategory.ts
@@ -1,10 +1,11 @@
 import { instance } from '@/shared/lib/instance';
 import type { ScoreByCategoryResponse } from '../model/types';
+import { ScoreStatus } from '@/entities/score/model/score';
 
-export const getScoresByCategory = async (memberId: number): Promise<ScoreByCategoryResponse> => {
+export const getScoresByCategory = async ({ memberId, status }: { memberId: number; status: ScoreStatus }): Promise<ScoreByCategoryResponse> => {
   const res = await instance.get(`/scores/by-category/${memberId}`, {
     params: {
-      status: 'APPROVED',
+      status,
     },
   });
   return res.data?.data ?? { categories: [] };
