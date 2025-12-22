29111ea841fb7c0d0f98d6852035873c0cb564a7
Dino0204 <s24020@gsm.hs.kr>
2025-12-23 02:38:09 +0900

    feat: 현재 선생님 정보 API

---
+++ src/entities/member/model/useGetCurrentMember.ts
@@ -0,0 +1,11 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { getCurrentMember } from '../api/getCurrentMember';
+
+export const useGetCurrentMember = () => {
+  return useQuery({
+    queryKey: ['student', 'current'],
+    queryFn: () => getCurrentMember(),
+    staleTime: 60 * 60 * 1000,
+  });
+};
