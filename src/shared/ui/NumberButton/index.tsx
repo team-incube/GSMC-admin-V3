df8b7bfea21d5377cd5773ada553fedea83370b5
Dino0204 <s24020@gsm.hs.kr>
2025-12-23 11:17:03 +0900

    refactor: className을 명시적으로 갖도록 변경

--- src/shared/ui/NumberButton/index.tsx
+++ src/shared/ui/NumberButton/index.tsx
@@ -1,18 +1,21 @@
+import { cn } from "@/shared/lib/cn";
+
 interface NumberButtonProps {
   value: number;
   onClick?: () => void;
   isSelected?: boolean;
+  className?: string;
 }
 
-export default function NumberButton({ value, onClick, isSelected = false }: NumberButtonProps) {
+export default function NumberButton({ value, onClick, isSelected = false, className, ...props }: NumberButtonProps & React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       onClick={onClick}
-      className={`h-13 w-23.5 cursor-pointer rounded-xl border px-4.5 py-3 text-lg font-bold ${
-        isSelected
-          ? 'bg-main-500 border-main-500 text-white'
-          : 'border-main-500 text-main-500 hover:bg-main-50'
-      }`}
+      className={cn(`h-13 w-23.5 cursor-pointer rounded-xl border px-4.5 py-3 text-lg font-bold ${isSelected
+        ? 'bg-main-500 border-main-500 text-white'
+        : 'border-main-500 text-main-500 hover:bg-main-50'
+        }`, className)}
+      {...props}
     >
       {value}
     </button>
