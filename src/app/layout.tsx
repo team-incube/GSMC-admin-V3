81cea2c62ff23d29195191783b739a93e4e6454d
Dino0204 <s24020@gsm.hs.kr>
2025-12-24 00:49:28 +0900

    fix: metatag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,9 @@ const pretendard = localFont({
 })
 
 export const metadata: Metadata = {
-  title: 'GSMC',
+  title: 'GSMC_ADMIN',
   description:
-    'GSMC 프로젝트는 GSM Certification의 약자로 GSM 인증제라는 뜻을 가지고 있습니다. 쓰기 복잡하고 점수관리가 어렵던 GSM 인증제를 선생님과 학생들 모두 더욱 더 쉽고 간편하게 웹사이트에서 관리하기 위해 개발하고 있는 프로젝트입니다.',
+    'GSMC_ADMIN 페이지는 마이스터부, 담임 선생님 분들을 위한 통합 관리 페이지입니다.',
 };
 
 export default function RootLayout({
