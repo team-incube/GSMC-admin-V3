e88ceccc8751e3ef6bdbc478f6e5ca2535d9c143
Dino0204 <s24020@gsm.hs.kr>
2025-12-23 11:31:01 +0900

    fix: 조건부 클래스 적용

--- src/shared/ui/NumberButton/index.tsx
+++ src/shared/ui/NumberButton/index.tsx
@@ -11,10 +11,13 @@ export default function NumberButton({ value, onClick, isSelected = false, class
   return (
     <button
       onClick={onClick}
-      className={cn(`h-13 w-23.5 cursor-pointer rounded-xl border px-4.5 py-3 text-lg font-bold ${isSelected
-        ? 'bg-main-500 border-main-500 text-white'
-        : 'border-main-500 text-main-500 hover:bg-main-50'
-        }`, className)}
+      className={cn(
+        'h-13 w-23.5 cursor-pointer rounded-xl border px-4.5 py-3 text-lg font-bold',
+        isSelected
+          ? 'bg-main-500 border-main-500 text-white'
+          : 'border-main-500 text-main-500 hover:bg-main-50',
+        className
+      )}
       {...props}
     >
       {value}
