173374fa79e586af94621dfca0d946c52a48bfc9
Dino0204 <s24020@gsm.hs.kr>
2025-12-31 16:14:43 +0900

    refactor: 모달 래퍼

--- src/shared/ui/ModalWrapper/index.tsx
+++ src/shared/ui/ModalWrapper/index.tsx
@@ -1,17 +1,18 @@
-import { cn } from '@/shared/lib/cn';
+import { cn } from "@/shared/lib/cn";
+import { useEffect } from "react";
 
-export default function ModalWrapper({
-  children,
-  className = '',
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) {
+export default function ModalWrapper({ children, className, onClose }: { children: React.ReactNode, className?: string, onClose?: () => void }) {
+
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, []);
+
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-[rgba(17,17,17,0.20)]">
-      <div className={cn('relative z-10 rounded-[20px] bg-white px-10 py-6', className)}>
-        {children}
-      </div>
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-[rgba(17,17,17,0.20)]" onClick={onClose}>
+      <div className={cn(`relative z-10 rounded-[20px] bg-white px-10 py-6`, className)} onClick={(e) => e.stopPropagation()}>{children}</div>
     </div>
   );
 }
