3f7dd1338e9f290f407dd38aaf2d7496ce5d121f
Dino0204 <s24020@gsm.hs.kr>
2025-12-31 11:50:19 +0900

    chore: type

--- src/entities/score/api/getScoresByCategory.ts
+++ src/entities/score/api/getScoresByCategory.ts
@@ -1,13 +1,15 @@
 import { instance } from '@/shared/lib/instance';
 import { CategoryScoresGroupType, ScoreStatus } from '@/entities/score/model/score';
 
+export interface getScoresByCategoryRequest {
+  memberId: number;
+  status?: ScoreStatus;
+}
+
 export const getScoresByCategory = async ({
   memberId,
   status,
-}: {
-  memberId: number;
-  status?: ScoreStatus;
-}): Promise<CategoryScoresGroupType[]> => {
+}: getScoresByCategoryRequest): Promise<CategoryScoresGroupType[]> => {
   const res = await instance.get(`/scores/by-category/${memberId}`, {
     params: {
       status,
